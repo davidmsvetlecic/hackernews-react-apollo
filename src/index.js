4cfa8742705d866b65ae50af6d2ab0e9a61b9d35
David Svetlecic <DSvetlecic@cylance.com>
2019-03-01 16:23:52 -0600

    Routing Add Header Go / on New Link

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/App'
+import { BrowserRouter } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
@@ -18,9 +19,11 @@ const client = new ApolloClient({
 })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
 serviceWorker.unregister();
